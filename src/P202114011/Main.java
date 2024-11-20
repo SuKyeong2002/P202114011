d6fc895785f2f4fe03ec9ef9548c5f4900520d94
Oh, Sukyeong <osukyeong0109@gmail.com>
2024-11-20 09:29:47 +0900

    메시지 two

--- src/P202114011/Main.java
+++ src/P202114011/Main.java
@@ -3,5 +3,7 @@ package P202114011;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("202114011 오수경");
+		System.out.println(Hello.sum(10));
+		System.out.println(Hello.sum(100));
 	}
 }
