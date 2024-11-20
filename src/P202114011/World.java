28396d13601c55e6d3cafe7804962519849cc179
Oh, Sukyeong <osukyeong0109@gmail.com>
2024-11-20 09:52:02 +0900

    메시지 four

---
+++ src/P202114011/World.java
@@ -0,0 +1,11 @@
+package P202114011;
+
+public class World {
+	public static long factorial(int n) {
+		long result = 1;
+		for (int i = 1; i <= n; ++i) {
+			result *= 1;
+		}
+		return result;
+	}
+}
